0560bbe2894d4dc9174f375b312a135905ce7406
Delemangi <milev.stefan@gmail.com>
2026-03-09 03:10:10 +0100

    Update project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {
+  base,
+  browser,
+  perfectionist,
+  prettier,
+  react,
+  typescript,
+} from 'eslint-config-imperium';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+export default [
+  { ignores: ['dist', 'vite.config.ts'] },
+  base,
+  browser,
+  react,
+  typescript,
+  prettier,
+  perfectionist,
+];
